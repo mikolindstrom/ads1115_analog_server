5bfb340ab43952a2cc79de06872feb86d9007c38
mikolindstrom <mfajarw@email.unikom.ac.id>
2024-06-21 11:14:08 +0700

    Update ads1115_analog_server.py

--- ads1115_analog_server.py
+++ ads1115_analog_server.py
@@ -18,9 +18,10 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create an ADS1115 object
 ads = ADS.ADS1115(i2c)
 
-channel=[]
-baca=[]
-nilai=[]
+analog_value1 = 0
+analog_value2 = 0
+analog_value3 = 0
+analog_value4 = 0
 
 class Ads1115_analog_server(NeuronModule):
     def __init__(self, **kwargs):
@@ -29,27 +30,22 @@ class Ads1115_analog_server(NeuronModule):
 
         if self.readChannel is None:
             raise InvalidParameterException("analog channel must be provided")
-        self.assign_variables()
+        
+        channel0 = AnalogIn(ads, ADS.P0)
+        analog_value1 = channel0.value        
+        nilai1 = str(analog_value1)
+        channel1 = AnalogIn(ads, ADS.P1)
+        analog_value2 = channel1.value        
+        nilai2 = str(analog_value2)
+        channel2 = AnalogIn(ads, ADS.P2)
+        analog_value3 = channel2.value        
+        nilai3 = str(analog_value3)
+        channel3 = AnalogIn(ads, ADS.P3)
+        analog_value4 = channel3.value        
+        nilai4 = str(analog_value4)
+        #self.say(nilai4)
+        #logger.info(nilai4)
 
-    def assign_variables(self):
-        for i, number in enumerate(self.readChannel):
-            setattr(self, f'number_{i+1}', number)
-            channel.append(number)
-        self.display_results()
-        
-    def display_results(self):
-        for i in range(len(self.numbers)):
-            nilai.append = AnalogIn(ads, channel[i])
-            baca.append = nilai[i].value
-
-        nilai1 = str(baca[0])
-        nilai2 = str(baca[1])
-        nilai3 = str(baca[2])
-        nilai4 = str(baca[3])
-        content =http.request("http://xxx.xxx.xxx.xxx/datasensor/terimasuhu.php?sensor1="+nilai1+"&sensor2="+nilai2, +"&sensor3="+nilai3,+"&sensor4="+nilai4, method="GET")[1]
-    
-        logger.info(message)
-        
-        
-        time.sleep(0.2)
+        content =http.request("http://xxx.xxx.xxx.xxx/datasensor/terimaanalog.php?sensor1="+nilai1+"&sensor2="+nilai2+"&sensor3="+nilai3+"&sensor4="+nilai4, method="GET")[1]
+        time.sleep(1)
 
